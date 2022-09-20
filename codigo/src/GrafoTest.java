b47f334166c2466790853d66a20f8ae4c7fc2a09
Paganini <paganini.xxi@gmail.com>
2022-09-20 13:42:13 -0300

    "Test class, file format csv"

---
+++ codigo/src/GrafoTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GrafoTest {
+    
+    public static Grafo grafo;
+
+    @BeforeEach
+    public void init(){
+        grafo = new Grafo("Test");
+    }
+
+
+    @Test
+    public void testeCarregarGrafo(){
+        
+    }
+}
