d2f969764b97a452c25f2ab08fbac9297304d442
MatheusMOAndrade <andradematheusmo@gmail.com>
2022-09-21 00:29:12 -0300

    att estrutura uml

---
+++ codigo/src/GrafoMutavel.java
@@ -0,0 +1,27 @@
+public abstract class GrafoMutavel extends Grafo {
+    
+    public boolean addAresta(int origem, int destino){
+
+    }
+
+    public boolean addVertice(int id){
+
+    }
+
+    public void carregar(Vertice novo) {
+
+    }
+
+    public boolean delAresta(int origem, int destino) {
+
+    }
+
+    public boolean delVertice(int idVertice) {
+
+    }
+
+    public void salvar(String nomeArquivo) {
+
+    }
+
+}
