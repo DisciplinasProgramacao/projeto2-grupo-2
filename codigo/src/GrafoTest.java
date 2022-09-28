98ef684ab2d2e2670582493839477a3a365c9a57
brunobgf <brunobgf40@gmail.com>
2022-09-28 16:49:53 -0300

    adding tests

--- codigo/src/GrafoTest.java
+++ codigo/src/GrafoTest.java
@@ -1,19 +1,72 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 public class GrafoTest {
 
-    public static Grafo grafo;
+    /*
+     * PLANO DE TESTES DA CLASSE MICROWAVE
+     * - Carregar um novo grafo 
+     * - Gerar um grafo completo
+     *   - Verificar se é um grafo completo
+     * - Tamanho de um grafo 
+     * - Subgrafo completo
+     */
+
+    static GrafoPonderado grafoPonderado;
+    static GrafoCompleto grafoCompleto;
 
     @BeforeEach
-    public void init() {
-        grafo = new GrafoNaoPonderado("New");
+    public void init(){
+        grafoPonderado = new GrafoPonderado("Grafo ponderado");
+        
     }
 
     @Test
-    public void testeCarregarGrafo() {
-        
+    public void carregaUmArquivoCsvParaLeitura(){
+        grafoPonderado.carregar("grafos.csv");
+        int existe=0;
+        if(grafoPonderado.existeVertice(4) != null){
+            existe++;
+        }
+        assertEquals(1, existe);
     }
+
+    @Test
+    public void criarGrafoCompleto() {
+        int grafoCriado = 0;
+        grafoCompleto = new GrafoCompleto("Grafo completo", 4);
+        if(grafoCompleto.completo()){
+            grafoCriado++;
+        }
+        assertEquals(1,grafoCriado);
+    }
+
+    @Test
+    public void gerarSubGrafoCompleto(){
+        int subGrafoCriado = 0;
+        Lista<Vertice> vertices = new Lista<Vertice>();
+        Vertice v1 = new Vertice(4);
+        Vertice v2 = new Vertice(3);
+        vertices.add(v1);
+        vertices.add(v2);
+
+        grafoCompleto = new GrafoCompleto("Grafo completo", 4);
+
+        grafoCompleto.subGrafo(vertices);
+
+    }
+
+
+
+
+
+
+
+    
+
 
 }
